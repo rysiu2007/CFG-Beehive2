45e20d416bf5ecb9b5f5eeed145a34b5312418c8
rysiu2007 <rysiu2007-2007@wp.pl>
2022-11-07 16:16:37 +0100

    Login

--- CFG Beehive/LoginPage.xaml.cs
+++ CFG Beehive/LoginPage.xaml.cs
@@ -6,16 +6,33 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Net.Http;
 
 namespace CFG_Beehive
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
+        string login, password,host = "", api = "/api/auth/login";
         public LoginPage()
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+            
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(host+api);
+            login = Login.Text;
+            password = Password.Text;
+            StringContent httpContent = new StringContent($"{{ \"username\": \"{login}\", \"password\": \"{password}\"}}");
+            HttpResponseMessage httpResponse = await client.PostAsync(host+api,httpContent);
+            if(httpResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Navigation.PopAsync(true);
+            }
         }
     }
 }
